522a823677a8cb22e2b3fc00613cbbcf85ec2a5e
Josh Avina <josh.avina78@gmail.com>
2017-03-27 16:04:10 -0700

    added associations for plans/users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327224745) do
+ActiveRecord::Schema.define(version: 20170327230049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
